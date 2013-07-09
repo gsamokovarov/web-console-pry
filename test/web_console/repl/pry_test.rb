7eac6b994c60c69f3cb3534cddecd09775f8dbb2
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-07-09 10:30:23 +0300

    Refactor the test return prompt format

--- test/web_console/repl/pry_test.rb
+++ test/web_console/repl/pry_test.rb
@@ -7,25 +7,25 @@ class PryTest < ActiveSupport::TestCase
   end
 
   test 'sending input returns the result as output' do
-    assert_equal sprintf(return_prompt, "42\n"), @pry.send_input('foo = 42')
+    assert_equal sprintf(return_prompt, '42'), @pry.send_input('foo = 42')
   end
 
   test 'session preservation requires same bindings' do
-    assert_equal sprintf(return_prompt, "42\n"), @pry1.send_input('foo = 42')
-    assert_equal sprintf(return_prompt, "42\n"), @pry2.send_input('foo')
+    assert_equal sprintf(return_prompt, '42'), @pry1.send_input('foo = 42')
+    assert_equal sprintf(return_prompt, '42'), @pry2.send_input('foo')
   end
 
   test 'session isolation requires own bindings' do
     pry1 = WebConsole::REPL::Pry.new(Object.new.instance_eval('binding'))
     pry2 = WebConsole::REPL::Pry.new(Object.new.instance_eval('binding'))
-    assert_equal sprintf(return_prompt, "42\n"), pry1.send_input('foo = 42')
+    assert_equal sprintf(return_prompt, '42'), pry1.send_input('foo = 42')
     assert_match undefined_var_or_method('foo'), pry2.send_input('foo')
   end
 
   test 'multi-line support' do
-    assert_equal "", @pry.send_input('class A')
-    assert_equal sprintf(return_prompt, "nil\n"), @pry.send_input('end')
-    assert_equal sprintf(return_prompt, "A\n"), @pry.send_input('A')
+    assert_equal '', @pry.send_input('class A')
+    assert_equal sprintf(return_prompt, 'nil'), @pry.send_input('end')
+    assert_equal sprintf(return_prompt, 'A'), @pry.send_input('A')
   end
 
   test "prompt isn't nil" do
@@ -40,7 +40,7 @@ class PryTest < ActiveSupport::TestCase
 
   private
     def return_prompt
-      "=> %s"
+      "=> %s\n"
     end
 
     def undefined_var_or_method(name)
